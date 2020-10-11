f47c56a3300c0f501667a91afcce3db8c448edad
Maryia Dronets <maryia.dronets@darkmatter.ae>
2020-10-11 14:37:01 +0300

    Fixed 'if' statements

--- src/main/java/by/tms/controller/HomeController.java
+++ src/main/java/by/tms/controller/HomeController.java
@@ -1,10 +1,13 @@
 package by.tms.controller;
 
+import by.tms.entity.AuthorizedUser;
+import by.tms.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
